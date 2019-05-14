47c8bc8fd7329a735b36d4bcef6e1f469d84299d
Stanley <snicholl@gmail.com>
2019-05-14 14:19:33 +0100

    Update semantics

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Main, Header } from "../Styles/Styled";
 
 
 const Layout = ({ children }) => (
-	<>
+	<Fragment>
 		<Header>
 			<img src="/logo.svg" alt="Grabyo logo" width="50" style={{ verticalAlign: "middle" }} />
 			<h1>
@@ -13,7 +13,7 @@ const Layout = ({ children }) => (
 		<Main role="main">
 			{children}
 		</Main>
-	</>
+	</Fragment>
 );
 
 export default Layout;
